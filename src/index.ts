f8303254c2f750902e8fe56b478984f99b6b66af
statiolake <statiolake@gmail.com>
2024-03-18 02:40:16 +0900

    fix: remove coc- prefix from command and channel name

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,10 @@ import {
 import VimSelect from './lists';
 
 export async function activate(context: ExtensionContext): Promise<void> {
-  const output = window.createOutputChannel('coc-list-vim-select');
+  const output = window.createOutputChannel('list-vim-select');
   const vimSelectList = new VimSelect(workspace.nvim);
   context.subscriptions.push(
-    commands.registerCommand('coc-list-vim-select.start', async (labels) => {
+    commands.registerCommand('list-vim-select.start', async (labels) => {
       vimSelectList.setItems(labels);
       output.appendLine('labels: ' + JSON.stringify(labels));
       workspace.nvim.command('CocList ' + vimSelectList.name);
